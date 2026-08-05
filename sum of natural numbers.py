03a453c1c7c9d64b0fe8f2d7d67e602b1efb0adc
dollysavita23-DS <dollysavita.23@gmail.com>
2026-08-05 13:21:26 +0530

    Add files via upload

---
+++ sum of natural numbers.py
@@ -0,0 +1,12 @@
+# Print sum of Natural Numbers
+
+n = int(input("Enter the value of n :"))
+
+sum = 0
+num = 1
+
+while num <= n:
+    sum = sum + num
+    num = num + 1
+print("Sum of Natural Numbers=",sum)
+        
