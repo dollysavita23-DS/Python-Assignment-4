f8995e5eb146a30b8c1c8379b0b614976cd579c5
dollysavita23-DS <dollysavita.23@gmail.com>
2026-08-11 11:57:19 +0530

    Add files via upload

---
+++ square root of the number.py
@@ -0,0 +1,14 @@
+# keep printing square of the number(stop when user enters a negative number)
+#using while loop
+
+while True:
+    n = int(input("Enter a number:"))
+    if(n>0) :
+        sqrt = n*n
+        print(sqrt)
+    elif(n<0):
+        print("stop")
+    
+    
+
+    
