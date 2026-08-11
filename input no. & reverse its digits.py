d8228e986d5b4018df3f1b8fbaab832811907569
dollysavita23-DS <dollysavita.23@gmail.com>
2026-08-11 20:45:32 +0530

    Add files via upload

---
+++ input no. & reverse its digits.py
@@ -0,0 +1,12 @@
+# Input a number and reverse its digits(using while loop)
+
+num = int(input("Enter a number:"))
+reverse = 0
+
+while num > 0 :
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+    print("Reverse of the number =",reverse)
+    
+
