66b455ed9afb7f0471af48b2c357c3fbfe231946
dollysavita23-DS <dollysavita.23@gmail.com>
2026-08-11 11:20:29 +0530

    Add files via upload

---
+++ Factorial of a number.py
@@ -0,0 +1,9 @@
+# To calculate the factorial of a number (using while loop)
+
+n = int(input("Enter a number:"))
+fact = 1
+i = 1
+while(i<=n):
+    fact = fact*i
+    i = i+1
+print("Factorial is :",fact)
