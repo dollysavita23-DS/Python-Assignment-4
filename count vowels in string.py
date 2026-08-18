887b07655ca88b6e9e9051d675dd8dbfc91ed6a6
dollysavita23-DS <dollysavita.23@gmail.com>
2026-08-18 10:52:00 +0530

    Add files via upload

---
+++ count vowels in string.py
@@ -0,0 +1,9 @@
+# To count Vowels in a string
+
+string = input("Enter a string:")
+vowels = "aeiou"
+count = 0
+for char in string:
+    if char in vowels:
+        count += 1
+print("Number of vowels:",count)
